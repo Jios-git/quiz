68e0e2f461a1d299ccd1c3eed885918efad43e15
jios-git <ethancampbell@live.co.uk>
2022-04-20 12:14:07 +0100

    commit before deleting v2 files of visualizer

--- audioquiz.js
+++ audioquiz.js
@@ -1,110 +1,50 @@
-(function () {
-  var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-    window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-  window.requestAnimationFrame = requestAnimationFrame;
-})();
+// Create a new instance of an audio object and adjust some of its properties
+var audio = new Audio();
+audio.src = 'audio/Fall.mp3';
+audio.loop = true;
+var btn = document.getElementById("audio-btn");
 
-function buttonClick(){
-  var btn = document.getElementById("audio-btn");
-  btn.classList.toggle("pause")
-  btn.onclick = ''
-}
-
-/*function buttonClick(){
-  var btn = document.getElementById("yes");
-  var state;
-  console.log(state)
-  if (state == false) {
-    btn.innerHTML = `<button id="audio-btn" class='audio-button' onclick="buttonClick();audioLoad();"></button>`
-    state = true
-    console.log('hi')
-  } else {
-    btn.innerHTML = `<button id="audio-btn" class='audio-button' onclick="buttonClick();"></button>`
-    state = false
+function togglePlay() {
+  if (audio.paused) {
+    audio.play()
+    btn.classList.toggle("pause")
   }
-  console.log(btn.innerHTML)
-  console.log(state)
-}*/
-
-function audioLoad() {
-  // Create a new `audioContext` and its `analyser`
-  var audioCtx = new AudioContext()
-
-  var analyser = audioCtx.createAnalyser()
-  // If a sound is still playing, stop it.
-  if (window.source)
-    source.noteOff(0)
-  // Decode the data in our array into an audio buffer
-  var audioFile = fetch("audio/seraqueen.mp3").then(
-    response => response.arrayweBuffer()).then(
-      buffer => audioCtx.decodeAudioData(buffer)).then(buffer => {
-        // Use the audio buffer with as our audio source
-        window.source = audioCtx.createBufferSource()
-        source.buffer = buffer
-        // Connect to the analyser ...
-        source.connect(analyser)
-        // and back to the destination, to play the sound after the analysis.
-        analyser.connect(audioCtx.destination)
-        // Start playing the buffer.
-        source.start(0)
-        // Initialize a visualizer object
-        var viz = new simpleViz()
-        // Finally, initialize the visualizer.
-        new visualizer(viz['update'], analyser)
-      })
-}
-
-// The visualizer object. 
-// Calls the `visualization` function every time a new frame
-// is available.
-// Is passed an `analyser` (audioContext analyser).
-function visualizer(visualization, analyser) {
-  var self = this
-  this.visualization = visualization
-  var last = Date.now()
-  var loop = function () {
-    var dt = Date.now() - last
-    // we get the current byteFreq data from our analyser
-    var byteFreq = new Uint8Array(analyser.frequencyBinCount)
-    analyser.getByteFrequencyData(byteFreq)
-    last = Date.now()
-    // We might want to use a delta time (`dt`) too for our visualization.
-    self.visualization(byteFreq, dt)
-    requestAnimationFrame(loop)
-  }
-  requestAnimationFrame(loop)
-}
-
-// A simple visualization. Its update function illustrates how to use 
-// the byte frequency data from an audioContext analyser.
-function simpleViz(canvas) {
-  var self = this
-  this.canvas = document.getElementById('canvas')
-  this.ctx = this.canvas.getContext("2d")
-  this.ctx.fillStyle = '#fff'
-  this.barWidth = 10
-  this.barGap = 5
-  //this.barHeight = 100
-  // We get the total number of bars to display
-  this.bars = Math.floor(this.canvas.width / (this.barWidth + this.barGap))
-  // This function is launched for each frame, together with the byte frequency data.
-  this.update = function (byteFreq) {
-    self.ctx.clearRect(0, 0, self.canvas.width, self.canvas.height)
-    // We take an element from the byteFreq array for each of the bars.
-    // Let's pretend our byteFreq contains 20 elements, and we have five bars...
-    var step = Math.floor(byteFreq.length / self.bars)
-    // `||||||||||||||||||||` elements
-    // `|   |   |   |   |   ` elements we'll use for our bars
-    for (var i = 0; i < self.bars; i++) {
-      // Draw each bar
-      var barHeight = byteFreq[i * step]/2
-      self.ctx.fillRect(
-        i * (self.barWidth + self.barGap),
-        self.canvas.height - barHeight,
-        self.barWidth,
-        barHeight)
-    }
+  else {
+    audio.pause()
+    btn.classList.toggle("pause")
   }
 }
 
-//https://github.com/cbrandolino/local-audio-visualizer
+// Establish all variables that your Analyser will use
+var canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_width, bar_height;
+// Initialize the MP3 player after the page loads all of its HTML into the window
+window.addEventListener("load", initMp3Player, false);
+function initMp3Player() {
+  document.getElementById('audio_container').appendChild(audio);
+  context = new AudioContext(); // AudioContext object instance
+  analyser = context.createAnalyser(); // AnalyserNode method
+  canvas = document.getElementById('analyser-render');
+  ctx = canvas.getContext('2d');
+  // Re-route audio playback into the processing graph of the AudioContext
+  source = context.createMediaElementSource(audio);
+  source.connect(analyser);
+  analyser.connect(context.destination);
+  frameLooper();
+}
+// frameLooper() animates any style of graphics you wish to the audio frequency
+// Looping at the default frame rate that the browser provides(approx. 60 FPS)
+function frameLooper() {
+  window.requestAnimationFrame(frameLooper);
+  fbc_array = new Uint8Array(analyser.frequencyBinCount);
+  analyser.getByteFrequencyData(fbc_array);
+  ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+  ctx.fillStyle = '#fff'; // Color of the bars
+  bars = 60;
+  for (var i = 0; i < bars; i++) {
+    bar_x = i * 5;
+    bar_width = 3;
+    bar_height = -(fbc_array[i] / 2);
+    //  fillRect( x, y, width, height ) // Explanation of the parameters below
+    ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
+  }
+}
--- audiothingv2.js
+++ audiothingv2.js
@@ -1,37 +1,48 @@
 // Create a new instance of an audio object and adjust some of its properties
 var audio = new Audio();
-audio.src = 'audio/Fall.mp3';
-audio.controls = true;
+audio.src = 'audio/seraqueen.mp3';
 audio.loop = true;
-audio.autoplay = true;
+var btn = document.getElementById("audio-btn");
+
+function togglePlay() {
+	if (audio.paused) {
+		audio.play()
+		btn.classList.toggle("pause")
+	}
+	else {
+		audio.pause()
+		btn.classList.toggle("pause")
+	}
+}
+
 // Establish all variables that your Analyser will use
 var canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_width, bar_height;
 // Initialize the MP3 player after the page loads all of its HTML into the window
 window.addEventListener("load", initMp3Player, false);
-function initMp3Player(){
-	document.getElementById('audio_box').appendChild(audio);
+function initMp3Player() {
+	document.getElementById('audio_container').appendChild(audio);
 	context = new AudioContext(); // AudioContext object instance
 	analyser = context.createAnalyser(); // AnalyserNode method
-	canvas = document.getElementById('analyser_render');
+	canvas = document.getElementById('analyser-render');
 	ctx = canvas.getContext('2d');
 	// Re-route audio playback into the processing graph of the AudioContext
-	source = context.createMediaElementSource(audio); 
+	source = context.createMediaElementSource(audio);
 	source.connect(analyser);
 	analyser.connect(context.destination);
 	frameLooper();
 }
 // frameLooper() animates any style of graphics you wish to the audio frequency
 // Looping at the default frame rate that the browser provides(approx. 60 FPS)
-function frameLooper(){
+function frameLooper() {
 	window.requestAnimationFrame(frameLooper);
 	fbc_array = new Uint8Array(analyser.frequencyBinCount);
 	analyser.getByteFrequencyData(fbc_array);
 	ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-	ctx.fillStyle = '#00CCFF'; // Color of the bars
-	bars = 100;
+	ctx.fillStyle = '#fff'; // Color of the bars
+	bars = 60;
 	for (var i = 0; i < bars; i++) {
-		bar_x = i * 3;
-		bar_width = 2;
+		bar_x = i * 5;
+		bar_width = 3;
 		bar_height = -(fbc_array[i] / 2);
 		//  fillRect( x, y, width, height ) // Explanation of the parameters below
 		ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
